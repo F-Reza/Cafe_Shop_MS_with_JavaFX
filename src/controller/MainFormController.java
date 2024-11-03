682506c5148f0922c26d0dd2220e341118627694
Forhad Reza <forhadreza55@gmail.com>
2024-11-03 12:13:43 +0600

    Add Search by Inv Id, Search by Item

--- src/controller/MainFormController.java
+++ src/controller/MainFormController.java
@@ -64,6 +64,7 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -72,6 +73,7 @@ import javafx.util.Callback;
 import models.CartItem;
 import models.EmployeeDataModel;
 import models.InvoiceDataModel;
+import models.InvoiceItem;
 import models.UserDataModel;
 
 /**
@@ -83,6 +85,128 @@ public class MainFormController implements Initializable {
     private PreparedStatement prepare;
     private Statement statement;
     private ResultSet result;
+    private String inv_date;
+    private String inv_time;
+    
+    
+    @FXML private AnchorPane invoice_DataForm;
+    @FXML private Label invoice_ID;
+    @FXML private Label invoice_Date;
+    @FXML private Label invoice_Time;
+    @FXML private Label invoice_OrderType;
+    @FXML private Label invoice_ServedBy;
+    @FXML private Label invoice_BillBy;
+    @FXML private Label invoice_Subtotal;
+    @FXML private Label invoice_Discount;
+    @FXML private Label invoice_OtherCharge;
+    @FXML private Label invoice_GrandTotal;
+    @FXML private Label invoice_TotalQty;
+    @FXML private Text  invoice_Note;
+    
+    @FXML private TableView<InvoiceItem> invoice_ItemTableView;
+    @FXML private TableColumn<InvoiceItem, Integer> invoice_SN;
+    @FXML private TableColumn<InvoiceItem, String> invoice_ItemName;
+    @FXML private TableColumn<InvoiceItem, Double> invoice_ItemRate;
+    @FXML private TableColumn<InvoiceItem, Integer> invoice_ItemQty;
+    @FXML private TableColumn<InvoiceItem, Double> invoice_ItemAmount;
+    
+    public InvoiceDataModel getInvoiceById(int invID) {
+        InvoiceDataModel invoice = null;
+        String query = "SELECT * FROM invoices WHERE id = ?";
+
+        try {
+            db.getConnection();
+            prepare = db.connection.prepareStatement(query);
+            prepare.setInt(1, invID);
+            ResultSet resultSet = prepare.executeQuery();
+
+            if (resultSet.next()) {
+                invoice = new InvoiceDataModel(
+                    resultSet.getInt("id"),
+                    resultSet.getString("inv_id"),
+                    resultSet.getString("items"),
+                    resultSet.getDouble("subtotal"),
+                    resultSet.getDouble("discount"),
+                    resultSet.getDouble("others_charge"),
+                    resultSet.getDouble("grand_total"),
+                    resultSet.getInt("total_qty"),
+                    resultSet.getString("note"),
+                    resultSet.getString("order_type"),
+                    resultSet.getString("served_by"),
+                    resultSet.getString("bill_by"),
+                    resultSet.getString("payment_status"),
+                    resultSet.getDate("date")
+                );
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+        }
+
+        return invoice;
+    }
+    public void loadInvoiceDataById() {
+    InvoiceDataModel invoice = getInvoiceById(20);
+        if (invoice == null) {
+            
+            long millis = invoice.getDate().getTime();
+            SimpleDateFormat sdfD = new SimpleDateFormat("dd MMM yyyy");
+            SimpleDateFormat sdfT = new SimpleDateFormat("hh:mm:aa");  
+            java.sql.Date resultdate = new java.sql.Date(millis);
+            inv_date = sdfD.format(resultdate.getTime());
+            inv_time = sdfT.format(resultdate.getTime());
+        
+        
+            invoice_ID.setText("Invoice ID: "+invoice.getInvID());
+            invoice_Date.setText(inv_date);
+            invoice_Time.setText(inv_time);
+            invoice_OrderType.setText("Order Type: "+invoice.getOrderType());
+            invoice_ServedBy.setText("Served By: "+invoice.getServedBy());
+            invoice_BillBy.setText("Bill By: "+invoice.getBillBy());
+            invoice_Subtotal.setText(String.format("Subtotal: %.2f Tk", invoice.getSubTotal()));
+            invoice_Discount.setText("Discount: "+invoice.getDiscount());
+            invoice_OtherCharge.setText("Others Charge: "+invoice.getOthersCharge());
+            invoice_GrandTotal.setText(String.format("Grand Total: ৳ %.2f Tk", invoice.getGrandTotal()));
+            invoice_TotalQty.setText("Total Qty: "+invoice.getTotalQty().toString());
+            invoice_Note.setText("Note: "+invoice.getNote());
+        
+            String formattedOutput = "Pasta, 1, 8.49, 8.49\nPasta, 1, 8.49, 8.49\nPasta, 1, 8.49, 8.49\nPasta, 1, 8.49, 8.49\nPasta, 1, 8.49, 8.49\nPizza, 1, 3.99, 3.99\nPasta new, 1, 4.49, 4.49\nPizza new, 1, 3.99, 3.99";
+            populateTableView(formattedOutput);
+
+        }
+    } 
+    public void populateTableView(String formattedOutput) {
+        String[] rows = formattedOutput.split("\n");
+        ObservableList<InvoiceItem> data = FXCollections.observableArrayList();
+
+        int serialNumber = 1;
+        for (String row : rows) {
+            String[] fields = row.split(", ");
+            String itemName = fields[0];
+            int quantity = Integer.parseInt(fields[1]);
+            double rate = Double.parseDouble(fields[2]);
+            double amount = Double.parseDouble(fields[3]);
+
+            // Create InvoiceItem object and add to the list
+            data.add(new InvoiceItem(serialNumber++, itemName, rate, quantity, amount));
+        }
+
+        // Set the data to the TableView
+        invoice_ItemTableView.setItems(data);
+    }
+    @FXML
+    public void initializeZ() {
+        invoice_SN.setCellValueFactory(new PropertyValueFactory<>("serialNumber"));
+        invoice_ItemName.setCellValueFactory(new PropertyValueFactory<>("itemName"));
+        invoice_ItemRate.setCellValueFactory(new PropertyValueFactory<>("rate"));
+        invoice_ItemQty.setCellValueFactory(new PropertyValueFactory<>("quantity"));
+        invoice_ItemAmount.setCellValueFactory(new PropertyValueFactory<>("amount"));
+    }
+    
+//    private final DB db = new DB();
+//    private PreparedStatement prepare;
+//    private Statement statement;
+//    private ResultSet result;
     
     private Alert alert;
     
@@ -157,6 +281,8 @@ public class MainFormController implements Initializable {
     @FXML private Button drinksBtn;
     @FXML private Button packagesBtn;
     @FXML private Button othersBtn;
+    @FXML private Button allBtn;
+    @FXML private Button menu_srcClearBtn;
     @FXML private TableView<CartItem> menuTableView;
     @FXML private TableColumn<CartItem, String> menu_Col_SN;
     @FXML private TableColumn<CartItem, String> menu_Col_ItemName;
@@ -996,6 +1122,88 @@ public class MainFormController implements Initializable {
             }
         }
     }
+    
+    private void displayFilteredCards(ObservableList<ItemsDataModel> filteredItems) {
+        menuGridPane.getChildren().clear(); // Clear existing cards
+
+        int row = 0;
+        int column = 0;
+
+        for (int i = 0; i < filteredItems.size(); i++) {
+            try {
+                FXMLLoader load = new FXMLLoader();
+                load.setLocation(getClass().getResource("/view/cardItem.fxml"));
+                AnchorPane pane = load.load();
+                CardItemController cardC = load.getController();
+                cardC.setData(filteredItems.get(i));
+
+                // Event handler for individual pane clicks
+                final int index = i;
+                pane.setOnMouseClicked(event -> addToCart(filteredItems.get(index)));
+
+                if (column == 7) {
+                    column = 0;
+                    row++;
+                }
+
+                menuGridPane.add(pane, column++, row);
+                GridPane.setMargin(pane, new Insets(7.45));
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }  
+    private void filterByItemName(String srcItem) {
+        ObservableList<ItemsDataModel> filteredList = FXCollections.observableArrayList();
+
+        if (srcItem == null || srcItem.isEmpty()) {
+            filteredList.addAll(menuGetData());
+        } else {
+            for (ItemsDataModel item : menuGetData()) {
+                if (item.getItemsName().toLowerCase().contains(srcItem.toLowerCase())) {
+                    filteredList.add(item);
+                } else if(item.getCategory().toLowerCase().contains(srcItem.toLowerCase())) {
+                    filteredList.add(item);
+                }
+            }
+        }
+
+        displayFilteredCards(filteredList); 
+    }
+
+    public void filterItemData() {
+        menu_searchItem.textProperty().addListener((observable, oldValue, newValue) -> {
+            filterByItemName(newValue);
+        });
+        //mealsBtn.setOnAction(event -> filterByItemName("Meals"));
+        mealsBtn.setOnAction(event -> { 
+            filterByItemName("Meals");
+            menu_searchItem.clear(); 
+        });
+        drinksBtn.setOnAction(event -> { 
+            filterByItemName("Drinks");
+            menu_searchItem.clear(); 
+        });
+        packagesBtn.setOnAction(event -> { 
+            filterByItemName("Packages");
+            menu_searchItem.clear(); 
+        });
+        othersBtn.setOnAction(event -> { 
+            filterByItemName("Others");
+            menu_searchItem.clear(); 
+        });
+        allBtn.setOnAction(event -> { 
+            menu_searchItem.clear(); 
+            filterByItemName("");
+        });
+        menu_srcClearBtn.setOnAction(event -> { 
+            menu_searchItem.clear(); 
+            filterByItemName("");
+        });
+        
+    }
+    
     private void setupCartTable() {
         
         menu_Col_SN.setCellValueFactory(new PropertyValueFactory<>("serialNumber"));
@@ -1406,7 +1614,7 @@ public class MainFormController implements Initializable {
     //// END CULLECT BILL SECTION
    
     
-    //// START INVOICE SECTION
+    //// START INVOICE SECTION srcByInvId
     private void loadInvoiceData() {
         // Check if any label is null before proceeding
         if (totalInvoice != null && pendingInvoices != null && completeInvoices != null) {
@@ -1425,6 +1633,25 @@ public class MainFormController implements Initializable {
         //adminUserName.setText(invoice.getUserName());
         //userDate.setText(invoice.getDate().toString());
         }
+    }
+    
+    private void filterByInvoiceID(String invID) {
+        if (invID == null || invID.isEmpty()) {
+            invoice_TableView.setItems(invoiceListData); // Show all items if search is empty
+        } else {
+            ObservableList<InvoiceDataModel> filteredList = FXCollections.observableArrayList();
+            for (InvoiceDataModel invoice : invoiceListData) {
+                if (invoice.getInvID().contains(invID)) {
+                    filteredList.add(invoice);
+                }
+            }
+            invoice_TableView.setItems(filteredList); // Update the TableView with the filtered list
+        }
+    }
+    public void filterInvoiceData() {
+        srcByInvId.textProperty().addListener((observable, oldValue, newValue) -> {
+            filterByInvoiceID(newValue);
+        });
     }
     // MERGE ALL DATAS
     public ObservableList<InvoiceDataModel> invoiceDataList() {
@@ -1471,6 +1698,8 @@ public class MainFormController implements Initializable {
     private ObservableList<InvoiceDataModel> invoiceListData;
     public void invoiceShowData() {
         loadInvoiceData();
+        filterInvoiceData();
+        filterByInvoiceID(xInvID);
         invoiceListData = invoiceDataList();
         
         invoice_Col_SN.setCellValueFactory(new PropertyValueFactory<>("id"));
@@ -1521,6 +1750,7 @@ public class MainFormController implements Initializable {
                     
                     // Set up view button action
                     viewButton.setOnAction(event -> {
+                        loadInvoiceDataById(); 
                         try{
                             getViewInvoice();
                         } catch (IOException ex) {
@@ -1621,6 +1851,7 @@ public class MainFormController implements Initializable {
         }
     }
     public void getViewInvoice() throws IOException {
+
         // TO HIDE MAIN FORM 
         //signoutBtn.getScene().getWindow().hide();
         Parent root = FXMLLoader.load(getClass().getResource("/view/invoice.fxml"));
@@ -3167,6 +3398,7 @@ public class MainFormController implements Initializable {
         }
         loadAdminData(1);
         //displayUsername(); 
+        filterItemData();
         
         //dashboardDisplayNC();
         //dashboardDisplayTI();
@@ -3190,7 +3422,7 @@ public class MainFormController implements Initializable {
         //menuShowOrderData();
         //customersShowData();
         billDisplayCard();
-
+        
         
         invoiceShowData();
         setDynamicColumnWidthForInvoiceTable();
